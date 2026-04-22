6fbef6626afa5b076d5b158f1d2047f3913e1c8f
Rahul Varun Kotagiri <rahulvarun9441@gmail.com>
2026-04-22 23:34:45 +0530

    chapter-5

---
+++ Chapter-5(Statements)/script.js
@@ -0,0 +1,25 @@
+//Expression
+//Expressions is a combination of values, variables, operators and functions that produces a value.
+let a = 10; // This is an expression that assigns the value 10 to the variable a.
+
+
+//Literals
+//Literals are fixed values that we can use in our code. They can be of different types such as string, number, boolean, null and undefined.
+let b = "Hello, World!"; // This is a string literal
+let c = true; // This is a boolean literal
+let d = null; // This is a null literal
+let e = undefined; // This is an undefined literal
+
+//All Literals are expressions 
+
+let ramuAmount = 2000; 
+let rahulAmount = 3000;
+
+let totalAmount = ramuAmount + rahulAmount; 
+
+console.log(`Total amount: ${totalAmount}`); 
+console.log(`Ramu's Amount: ${ramuAmount}`);
+console.log(`Rahul's Amount: ${rahulAmount}`);
+console.log(`Total Amount: ${ramuAmount + rahulAmount}`);
+
+// to make expression executable in string we can use template literals which are enclosed in backticks `` and we can embed expressions inside ${}.
